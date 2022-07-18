9fb3d3772966a127bea8659167a5c58eeedc85e3
Kirill Banakh <kirill.banakh@outlook.com>
2022-07-18 21:49:50 +0300

    feat: Guessing game

--- source/rust_sandbox.rs
+++ source/rust_sandbox.rs
@@ -1,7 +1,10 @@
 mod hello_rust;
+mod guessing_game;
 
 use crate::hello_rust::greeting;
+use crate::guessing_game::ggame;
 
 fn main() {
     greeting::print_greeting();
+    ggame::ggame_start();
 }
